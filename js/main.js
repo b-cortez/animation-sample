d8d071eb0760f5371a421ca26ba674ef57a57ba7
Brian Cortez <brianolivares@me.com>
2019-07-31 16:06:20 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,106 @@
+/* ===================================================================
+ * Main JS
+ *
+ * ------------------------------------------------------------------- */
+var clSquare = function() {
+
+    $('a').hoverIntent(function() {
+        $(this).removeClass('active');
+        setTimeout(function(){
+          $(this).addClass('temp');
+        },500);
+      },
+      function() {
+        $(this).addClass('active');
+      $(this).removeClass('temp');
+      }
+    );
+  
+  };
+(function($) {
+
+    "use strict";
+    
+    var cfg = {
+        scrollDuration : 800, // smoothscroll duration
+    },
+
+    $WIN = $(window);
+
+    // Add the User Agent to the <html>
+    // will be used for IE10 detection (Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0))
+    var doc = document.documentElement;
+    doc.setAttribute('data-useragent', navigator.userAgent);
+
+
+   /* Preloader
+    * -------------------------------------------------- */
+    var clPreloader = function() {
+        
+        $("html").addClass('cl-preload');
+
+        $WIN.on('load', function() {
+
+            //force page scroll position to top at page refresh
+             $('html, body').animate({ scrollTop: 0 }, 'normal');
+
+            // will first fade out the loading animation 
+            $("#loader").fadeOut("slow", function() {
+                // will fade out the whole DIV that covers the website.
+                $("#preloader").delay(300).fadeOut("slow");
+            }); 
+            
+            // for hero content animations 
+            $("html").removeClass('cl-preload');
+            $("html").addClass('cl-loaded');
+        
+        });
+    };
+
+   /* Smooth Scrolling
+    * ------------------------------------------------------ */
+    var clSmoothScroll = function() {
+        
+        $('.smoothscroll').on('click', function (e) {
+            var target = this.hash,
+            $target    = $(target);
+            
+                e.preventDefault();
+                e.stopPropagation();
+
+            $('html, body').stop().animate({
+                'scrollTop': $target.offset().top
+            }, cfg.scrollDuration, 'swing').promise().done(function () {
+
+                window.location.hash = target;
+            });
+        });
+
+    };
+
+   /* Animate On Scroll
+    * ------------------------------------------------------ */
+    var clAOS = function() {
+        
+        AOS.init( {
+            offset: 200,
+            duration: 600,
+            easing: 'ease-in-sine',
+            delay: 300,
+            once: true,
+            disable: 'mobile'
+        });
+
+    };
+
+   /* Initialize
+    * ------------------------------------------------------ */
+    (function ssInit() {
+        clPreloader();
+        clSmoothScroll();
+        clAOS();
+        clSquare();
+    })();
+        
+        
+})(jQuery);
